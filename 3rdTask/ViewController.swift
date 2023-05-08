8923d0351f691f9ef4d5b41b65d84d6ba4e692ce
Ruslan Gaifullin <gayfullin.r.i@yandex.ru>
2023-05-08 20:18:35 +0300

    Initial Commit

---
+++ 3rdTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3rdTask
+//
+//  Created by Руслан Гайфуллин on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
